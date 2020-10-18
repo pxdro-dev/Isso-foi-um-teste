75f7a82b124daf234c3b16f6f8b0c50cf1a2f4bd
unknown <pepeu.oliveira491@gmail.com>
2020-10-18 13:24:04 -0300

    :package: JS orçamento !

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 document.querySelector(".hbg").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")   
 );
+document.querySelector("#qtde").addEventListener("change", AtualizarPreco)
+document.querySelector("#js").addEventListener("change", AtualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", AtualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", AtualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function (){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    AtualizarPreco ()
+})
+
+function AtualizarPreco(){
+    const quant = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value 
+
+    let preco = quant * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
